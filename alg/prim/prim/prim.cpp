2ee65034785eeadcf84b012b3c81501d82f1cfe3
yutianwu <wzxingbupt@gmail.com>
2014-02-28 12:17:23 +0800

    prim算法实现

--- alg/prim/prim/prim.cpp
+++ alg/prim/prim/prim.cpp
@@ -14,18 +14,18 @@ MST-PRIM（G, w, r）
 				do if v in Q and w(u, v) < key[v]
 					then pi[v] <- u
 						key[v] = w(u,v)
-
 */
 #include <iostream>
 #include <vector>
 using namespace std;
 
 #define INF 10000
-#define NIL 0
+#define NIL -1
 
 //节点
 struct Node {
-	Node(int vertex, int key = INF) : vertex(vertex), key(key) {}
+	Node(int vertex = 0, int key = INF) : vertex(vertex), key(key) {}
+
 	int vertex;
 	int key;
 };
@@ -33,7 +33,7 @@ struct Node {
 void swap(Node &a, Node &b) {
 	Node tmp = a;
 	a = b;
-	b = a;
+	b = tmp;
 }
 
 //对优先队列进行MIN_HEAPIFY操作，使队列保持最小堆的性质
@@ -49,26 +49,69 @@ void minHeapify(vector<Node> &Q, int i) {
 		min = right;
 	}
 	if (min != i) {
-		swap(Q[min], Q[i]);
+		swap(Q[min - 1], Q[i - 1]);
 		minHeapify(Q, min);
+	}
+}
+
+//取出优先队列中最小的
+Node extractMin(vector<Node> &Q) {
+	Node min = NULL;
+
+	if (Q.size() < 1) {
+		return min;
+	}
+	min = Q[0];
+	Q[0] = Q[Q.size() - 1];
+	Q.pop_back();
+	minHeapify(Q, 1);
+	
+	return min;
+}
+
+//DECERSE-KEY
+void decreaseKey(vector<Node> &Q, int i, int key) {
+	if (Q[i-1].key < key) {
+		return;
+	}
+
+	Q[i-1].key = key;
+	while (i > 1 && Q[i/2 - 1].key > key) {
+		swap(Q[i- 1], Q[i/2 - 1]);
+		i = i/2;
 	}
 }
 
 void prim(int adj[][4]) {
 	vector<Node> Q;	//queue
 	vector<int> P;  //parents
+
 	for (int i=0; i != 4; i++) {
 		Q.push_back(Node(i));
 		P.push_back(NIL);
 	}
 	Q[0].key = 0;
-	while (Q.size() > 0) {
 
+	while (Q.size() > 0) {
+		Node u = extractMin(Q);
+		for (vector<Node>::size_type j = 0; j != Q.size(); j++) {
+			if (Q[j].key > adj[u.vertex][Q[j].vertex]) {
+				P[Q[j].vertex] = u.vertex;
+				decreaseKey(Q, j+1, adj[u.vertex][Q[j].vertex]);
+			}
+		}
+	}
+
+	for (vector<int>::size_type j = 0; j != P.size(); j++) {
+		cout<<P[j]<<endl;
 	}
 }
 
 void main() {
 	int adj[4][4] = {{0,9,6,4},{9,0,7,INF},{6,7,0,3},{4,INF,3,0}}; //adjacency matrix
-
+	prim(adj);
+	vector<Node> node;
+	node.push_back(Node(1));
+	cout<<node[0].key;
 	system("PAUSE");
 }
